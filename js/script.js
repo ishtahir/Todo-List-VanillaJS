f98fae1fe85eb1a8fcaceb295555d4041d757395
Ish Tahir <ishtahir@icloud.com>
2018-09-12 00:37:54 -0500

    Now pushing objects to todo array, with text and completed props

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@ const todoList = {
     todo: [],
     displayTodos: function() {
         if (this.todo.length === 0) {
-            console.log('List is empty, please add an item to do')
+            console.log('List is empty, please add an item to do');
         }
-        console.log(this.todo)
+        this.todo.forEach(item => console.log(item));
     },
-    addTodo: function(newItem) {
-        this.todo.push(newItem);
+    addTodo: function(text) {
+        this.todo.push({
+            todoText: text,
+            completed: false
+        });
         this.displayTodos();
     },
     changeTodo: function(index, newValue) {
-        this.todo[index] = newValue;
+        this.todo[index].todoText = newValue;
         this.displayTodos();
     },
     deleteTodo: function(index) {
